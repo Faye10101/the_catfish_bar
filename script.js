74e7bc8125416d7818a71f5a57bf54685b77e6a5
Faye10101 <lhf060128@gmail.com>
2026-06-07 18:20:24 +1000

    Revert "make some changes"
    This reverts commit 514e22cd7ef019955b9489a0749d1749bb24bc9c.

--- script.js
+++ script.js
@@ -47,19 +47,17 @@ function showProducts(catagory) {
     const currentData = webData[catagory];
 
     function makeHtml(list) {
-    let htmlContent = '';
-    list.forEach(function(item) {
-        htmlContent += `
-            <a href="product.html?name=${encodeURIComponent(item.name)}" style="text-decoration: none;">
-                <div class="item-card" data-tag="${item.tag}" data-name="${item.name.toLowerCase()}">
-                    <img src="${item.img}" alt="${item.name}">
+        let htmlContent = '';
+        list.forEach(function(item) {
+            htmlContent += `
+                <div class='item-card' data-tag='${item.tag}' data-name='${item.name.toLowerCase()}'>
+                    <img src='${item.img}' alt='${item.name}'>
                     <p>${item.name}</p>
                 </div>
-            </a>
-        `;
-    });
-    return htmlContent;
-}
+            `;
+        });
+        return htmlContent;
+    }
 
     if (bestContainer) bestContainer.innerHTML = makeHtml(currentData.best);
     if (latestContainer) latestContainer.innerHTML = makeHtml(currentData.latest);
@@ -511,7 +509,7 @@ function setUpAccordions() {
     });
 }
 
-function setUpGoToCartButton(product) {
+function setUpGoToCartButton() {
     const goToCartButton = document.getElementById('go-to-cart-button');
     if (!goToCartButton) {
         return;
@@ -519,7 +517,7 @@ function setUpGoToCartButton(product) {
     goToCartButton.addEventListener('click', function() {
         const smallQuantity = Number(document.getElementById('quantity-small').textContent);
         const largeQuantity = Number(document.getElementById('quantity-large').textContent);
-
+        
         if (smallQuantity === 0 && largeQuantity === 0) {
             goToCartButton.textContent = 'Please choose a quantity first';
             setTimeout(function() {
@@ -527,28 +525,6 @@ function setUpGoToCartButton(product) {
             }, 1500);
             return;
         }
-
-        const itemsToAdd = [];
-        if (smallQuantity > 0) {
-            itemsToAdd.push({
-                productId: product.productId,
-                productName: product.productName,
-                sizeLabel: '285ML',
-                unitPrice: product.smallSizePrice,
-                quantity: smallQuantity
-            });
-        }
-        if (largeQuantity > 0) {
-            itemsToAdd.push({
-                productId: product.productId,
-                productName: product.productName,
-                sizeLabel: '570ML',
-                unitPrice: product.largeSizePrice,
-                quantity: largeQuantity
-            });
-        }
-
-        addItemsToCart(itemsToAdd);
         window.location.href = 'cart.html';
     });
 }
